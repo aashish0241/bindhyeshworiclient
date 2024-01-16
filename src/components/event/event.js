2f27b8efdb95d2c7fca55ecaa653c9ebd1bc078f
aashish0241 <127502118+aashish0241@users.noreply.github.com>
2024-01-16 11:30:57 +0545

    change

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Event() {
   const [posts, setPosts] = useState([]);
@@ -6,16 +6,16 @@ export default function Event() {
   const [selectedPostDetails, setSelectedPostDetails] = useState(null);
 
   useEffect(() => {
-    // Fetch data from the API, sorted by createdAt date
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/event/event');
+        const response = await fetch("http://localhost:3000/api/event/event");
         const data = await response.json();
-        // Sort the data by createdAt date in descending order
-        const sortedData = data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        const sortedData = data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
         setPosts(sortedData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -23,33 +23,33 @@ export default function Event() {
   }, []);
 
   useEffect(() => {
-    // Fetch selected post details when selectedPost changes
     if (selectedPost !== null) {
       fetch(`http://localhost:3000/api/event/event/${selectedPost}`)
-        .then(response => response.json())
-        .then(data => {
-          // Update the state with the fetched post details
+        .then((response) => response.json())
+        .then((data) => {
           setSelectedPostDetails(data);
         })
-        .catch(error => console.error('Error fetching post details:', error));
+        .catch((error) => console.error("Error fetching post details:", error));
     }
   }, [selectedPost]);
 
   const handlePostClick = (postId) => {
-    // Toggle visibility of description on click
     setSelectedPost(selectedPost === postId ? null : postId);
   };
 
   const truncateDescription = (description, limit) => {
-    const words = description.split(' ');
-    const truncatedText = words.slice(0, limit).join(' ');
+    const words = description.split(" ");
+    const truncatedText = words.slice(0, limit).join(" ");
 
     if (words.length > limit) {
       return (
         <>
           {truncatedText}
-          <span className="text-blue-600 cursor-pointer" onClick={() => handlePostClick(selectedPost)}>
-            {' Read More'}
+          <span
+            className="text-blue-600 cursor-pointer"
+            onClick={() => handlePostClick(selectedPost)}
+          >
+            {" Read More"}
           </span>
         </>
       );
@@ -62,17 +62,25 @@ export default function Event() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">School News and Notice</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            School News and Notice
+          </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             All school events are uploaded here. Check your events.
           </p>
         </div>
         <div className="mx-auto grid mt-10 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
-            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+            <article
+              key={post.id}
+              className="flex max-w-xl flex-col items-start justify-between"
+            >
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-500 text-xl font-bold">
-                  Post date:. {post.date}
+                <time
+                  dateTime={post.datetime}
+                  className="text-gray-500 text-xl font-bold"
+                >
+                  Post date: {post.date}
                 </time>
               </div>
               <div className="group relative bg-gray-200 bg-opacity-50 p-6 ">
@@ -82,14 +90,23 @@ export default function Event() {
                     {post.title}
                   </div>
                 </h3>
-                <img src={post.file} alt="image" className="mt-5 h-60 w-80  bg-gray-50" />
+                <img
+                  src={post.file}
+                  alt="logo"
+                  className="mt-5 h-60 w-80  bg-gray-50"
+                />
                 <p className="mt-5 text-sm leading-6 text-gray-600">
                   {truncateDescription(post.description, 40)}
                 </p>
                 {selectedPost === post.id && (
                   <>
-                    <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
-                    <p className="text-sm leading-6 font-semibold text-gray-900">{post.name}</p>
+                    <p className="mt-5 text-sm leading-6 text-gray-600">
+                      {post.description}
+                    </p>
+                    <p className="text-sm leading-6 font-semibold text-gray-900">
+                      {post.name}
+                    </p>
+                    {selectedPostDetails}
                   </>
                 )}
               </div>
