172ea1824ffb0714982f22f75884d725637247c2
aashish0241 <timsinaaashish6@gmail.com>
2024-01-18 15:06:28 +0545

    chnaged

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-export default function Event() {
+const Event = () => {
   const [posts, setPosts] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
   const [selectedPostDetails, setSelectedPostDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,24 +15,15 @@ export default function Event() {
           (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
         );
         setPosts(sortedData);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
-
-  useEffect(() => {
-    if (selectedPost !== null) {
-      fetch(`${process.env.REACT_APP_BACKEND_URL}/api/event/event/${selectedPost}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setSelectedPostDetails(data);
-        })
-        .catch((error) => console.error("Error fetching post details:", error));
-    }
-  }, [selectedPost]);
 
   const handlePostClick = (postId) => {
     setSelectedPost(selectedPost === postId ? null : postId);
@@ -69,51 +61,60 @@ export default function Event() {
             All school events are uploaded here. Check your events.
           </p>
         </div>
+
         <div className="mx-auto grid mt-10 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className="flex max-w-xl flex-col items-start justify-between"
-            >
-              <div className="flex items-center gap-x-4 text-xs">
-                <time
-                  dateTime={post.datetime}
-                  className="text-gray-500 text-xl font-bold"
-                >
-                  Post date: {post.date}
-                </time>
-              </div>
-              <div className="group relative bg-gray-200 bg-opacity-50 p-6 ">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <div onClick={() => handlePostClick(post.id)}>
-                    <span className="absolute inset-0 text-3xl font-bold flex items-center" />
-                    {post.title}
-                  </div>
-                </h3>
-                <img
-                  src={post.file}
-                  alt="logo"
-                  className="mt-5 h-60 w-80  bg-gray-50"
-                />
-                <p className="mt-5 text-sm leading-6 text-gray-600">
-                  {truncateDescription(post.description, 40)}
-                </p>
-                {selectedPost === post.id && (
-                  <>
-                    <p className="mt-5 text-sm leading-6 text-gray-600">
-                      {post.description}
-                    </p>
-                    <p className="text-sm leading-6 font-semibold text-gray-900">
-                      {post.name}
-                    </p>
-                    {selectedPostDetails}
-                  </>
-                )}
-              </div>
-            </article>
-          ))}
+          {isLoading ? (
+            <div className=" bg-blue items-center justify-center flex text-black">
+             <h1 className="text-black text-2xl font-bold">Wait  Data is Loading...</h1>
+            </div>
+          ) : (
+            posts.map((post) => (
+              <article
+                key={post.id}
+                className="flex max-w-xl flex-col items-start justify-between"
+              >
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time
+                    dateTime={post.datetime}
+                    className="text-gray-500 text-xl font-bold"
+                  >
+                    Post date: {post.date}
+                  </time>
+                </div>
+                <div className="group relative bg-gray-200 bg-opacity-50 p-6 ">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <div onClick={() => handlePostClick(post.id)}>
+                      <span className="absolute inset-0 text-3xl font-bold flex items-center" />
+                      {post.title}
+                    </div>
+                  </h3>
+                  <img
+                    src={post.file}
+                    alt="logo"
+                    className="mt-5 h-60 w-80  bg-gray-50"
+                  />
+                  <p className="mt-5 text-sm leading-6 text-gray-600">
+                    {truncateDescription(post.description, 40)}
+                  </p>
+                  {selectedPost === post.id && (
+                    <>
+                      <p className="mt-5 text-sm leading-6 text-gray-600">
+                        {post.description}
+                      </p>
+                      <p className="text-sm leading-6 font-semibold text-gray-900">
+                        {post.name}
+                      </p>
+                      {selectedPostDetails}
+                    </>
+                  )}
+                </div>
+              </article>
+            ))
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Event;
